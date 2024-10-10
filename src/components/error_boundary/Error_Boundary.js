c189454f1608873093d43e2f6eae69a4aa944c19
Bayel Erkinbekov <2001shermurzaev@gmail.com>
2024-10-10 22:39:16 +0600

    добавил error boundary

---
+++ src/components/error_boundary/Error_Boundary.js
@@ -0,0 +1,21 @@
+import { Component } from "react";
+import ErrorMessage from "../error_message/Error_message"
+
+class ErrorBoundary extends Component {
+    state = { error: false };
+
+    componentDidCatch(error, errorInfo) {
+        console.log(error, errorInfo);
+        this.setState({ error: true });
+    }
+
+    render() {
+        if (this.state.error === true) {
+            return <ErrorMessage />;
+        }
+
+        return this.props.children;
+    }
+}
+
+export default ErrorBoundary;
