35cb282d685e3ae5f7f5dea6b45b90f7eb256d72
Bayel Erkinbekov <2001shermurzaev@gmail.com>
2024-11-12 06:02:29 +0600

    cors update

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 const corsOptions = {
-    origin: 'https://mybaskets.online',
+    origin: ['https://mybaskets.online', 'https://www.mybaskets.online'],
     methods: ['GET', 'POST', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true 
